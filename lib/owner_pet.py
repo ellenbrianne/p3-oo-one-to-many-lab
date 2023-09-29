a71dde6bb5d42f244314d12e056f6d5c8fb1cdc3
Ellen Bennett <bennette831@gmail.com>
2023-09-29 12:53:10 -0600

    use object relationship/association to create Pet and Owner classes

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,45 @@
 class Pet:
-    pass
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    def __init__(self, name, pet_type, owner=""):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.add_instance(self)
+
+    def get_pet_type(self):
+        return self._pet_type
+                
+    def set_pet_type(self, pet_type):
+        if pet_type in self.PET_TYPES:
+            self._pet_type = pet_type
+        else:
+            raise Exception
+
+    pet_type = property(get_pet_type, set_pet_type)
+
+    @classmethod
+    def add_instance(cls, self):
+        cls.all.append(self)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+        
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception
+        
+    def get_sorted_pets(self):    
+        owned_pets = [pet for pet in Pet.all if pet.owner == self]
+        def get_name(obj):
+            return obj.name
+        owned_pets.sort(key=get_name)
+        return owned_pets
